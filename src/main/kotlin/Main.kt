1a0ae4041cde87f5e8aa138d7aa9a592cbb5a60f
limbuserendipity <limbuserendipity@gmail.com>
2024-10-14 18:26:18 +0700

    Add function sayAge

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,14 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     sayName("Alexey")
+    sayAge(26)
 }
 
 fun sayName(name : String){
     println("Привет, $name")
+}
+
+fun sayAge(age : Int){
+    println("Тебе, $age лет")
 }
 
